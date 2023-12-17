6360450f827b0ea0f95cfb00d9d9dcc30c2b4f99
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2023-12-17 08:01:45 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -148,7 +148,7 @@ cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-key
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
   sudo tee /etc/apt/sources.list.d/signal-xenial.list
 
-sudo apt update && sudo apt install signal-desktop
+sudo apt update && sudo apt install -y signal-desktop
 
 # video editor
 # sudo apt install -y kdenlive
