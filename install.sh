a70820567614a403765ef5fbc371a95b1f6e9ede
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2023-12-18 00:04:30 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,45 +2,11 @@
 sudo apt install -y ufw
 sudo ufw enable
 
-# user directories
-xdg-user-dirs-update
-
-# window manager
-sudo apt install -y i3
-
 # network manager
 sudo apt install -y network-manager-gnome
 
-# Notification Daemon
-sudo apt install -y dunst libnotify-bin
-
-# display manager
-sudo apt install -y lightdm
-sudo systemctl enable lightdm
-
-# display settings
-sudo apt install -y arandr
-
-# terminal emulators
-sudo apt install -y terminator konsole
-
-# admin web console
-sudo apt install cockpit
-
-# background manager
-sudo apt install -y feh
-
-# app launcher
-sudo apt install -y rofi
-
 # appearance managers
 sudo apt install -y lxappearance picom 
-
-# settings interface
-sudo apt install -y xfce4-settings xfce4-power-manager
-
-# auto numlock
-sudo apt install -y numlockx
 
 # file manager
 sudo apt install -y thunar
@@ -48,57 +14,30 @@ sudo apt install -y thunar
 # advanced file manager
 sudo apt install -y krusader
 
-# office apps
-sudo apt install -y libreoffice
+# settings interface
+sudo apt install -y xfce4-settings xfce4-power-manager
+
+# terminal emulators
+sudo apt install -y terminator konsole
 
 # audio
 sudo apt install -y pulseaudio alsa-utils pavucontrol volumeicon-alsa
+
+# terminal apps
+sudo apt install -y neofetch htop cmatrix glances figlet
 
 # enable repos
 sudo apt install -y software-properties-common apt-transport-https curl ca-certificates -y
 sudo apt-add-repository contrib non-free-firmware
 sudo apt update && sudo apt upgrade
 
-# non free codecssudo apt install -y dunst libnotify-bin
-sudo apt install -y ttf-mscorefonts-installer rar unrar libavcodec-extra gstreamer1.0-libav gstreamer1.0-plugins-ugly gstreamer1.0-vaapi
+# printer support
+#sudo apt install -y cups
+#sudo systemctl enable cups
 
-# disk utilities
-sudo apt install -y gnome-disk-utility gsmartcontrol gparted
-
-# gui text editor
-sudo apt install -y geany
-
-# terminal apps
-sudo apt install -y neofetch htop cmatrix glances figlet
-
-# screenshots
-sudo apt install -y maim xclip xdotool copyq
-
-# snap store
-sudo apt install snapd
-sudo snap install core
-
-# image editors
-sudo apt install -y gimp
-sudo snap install pinta
-
-# zip utilities
-sudo apt install -y tar gzip
-
-# media player
-sudo apt install -y vlc 
-
-# backup manager
-sudo apt install -y timeshift
-
-# notification daemon
-sudo apt install -y dunst libnotify-bin
-
-# gpg encryption manager
-sudo apt install -y kleopatra
-
-# password manager
-sudo apt install -y keepass2
+# bluetooth support
+#sudo apt install -y bluez blueman
+#sudo systemctl enable bluetooth
 
 # brave browser
 wget -qO- https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg | sudo gpg --dearmor | sudo tee /usr/share/keyrings/brave-browser-archive-keyring.gpg > /dev/null
@@ -121,26 +60,63 @@ Signed-By: /usr/share/keyrings/librewolf.gpg
 EOF
 
 sudo apt update
-sudo apt install librewolf -y
+sudo apt install -y librewolf
 
 # other browsers
 sudo apt install -y firefox-esr chromium
 
-# printer support
-#sudo apt install -y cups
-#sudo systemctl enable cups
+# background manager
+sudo apt install -y feh
 
-# bluetooth support
-#sudo apt install -y bluez blueman
-#sudo systemctl enable bluetooth
+# app launcher
+sudo apt install -y rofi
 
-# nordvpn
-sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh)
-sudo usermod -aG nordvpn $USER
-sh ~/deb12-i3/nordconfig.sh
+# auto numlock
+sudo apt install -y numlockx
 
-# import dot files
-cp -rT ~/deb12-i3/config ~/.config
+# Notification Daemon
+sudo apt install -y dunst libnotify-bin
+
+# admin web console
+sudo apt install -y cockpit
+
+# office apps
+sudo apt install -y libreoffice
+
+# display settings
+sudo apt install -y arandr
+
+# media player
+sudo apt install -y vlc 
+
+# non free codecssudo apt install -y dunst libnotify-bin
+sudo apt install -y ttf-mscorefonts-installer rar unrar libavcodec-extra gstreamer1.0-libav gstreamer1.0-plugins-ugly gstreamer1.0-vaapi
+
+# disk utilities
+sudo apt install -y gnome-disk-utility gsmartcontrol gparted
+
+# screenshots
+sudo apt install -y maim xclip xdotool copyq
+
+# snap store
+sudo apt install -y snapd
+sudo snap install -y core
+
+# image editors
+sudo apt install -y gimp
+sudo snap install -y pinta
+
+# zip utilities
+sudo apt install -y tar gzip
+
+# backup manager
+sudo apt install -y timeshift
+
+# gpg encryption manager
+sudo apt install -y kleopatra
+
+# password manager
+sudo apt install -y keepass2
 
 # signal
 wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
@@ -164,6 +140,22 @@ sudo apt install -y spice-vdagent
 # sudo usermod -aG libvirt “$(whoami)”
 # sudo usermod -aG kvm “$(whoami)”
 
+# nordvpn
+sh <\(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh\)
+sudo usermod -aG nordvpn $USER
+sh ~/deb12-i3/nordconfig.sh
+
+# user directories
+xdg-user-dirs-update
+
+# window manager
+sudo apt install -y i3
+
+# display manager
+sudo apt install -y lightdm
+sudo systemctl enable lightdm
+
+source ~/deb12-i3/copconf.sh
 
 sudo apt auto-remove -y
 
