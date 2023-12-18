6e26529bab242242875bd3805e734746c907fd56
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2023-12-18 17:54:46 -0500

    Update nordconfig.sh

--- nordconfig.sh
+++ nordconfig.sh
@@ -3,3 +3,4 @@ cp ~/deb12-i3/norddouble.sh ~/norddouble.sh
 cp ~/deb12-i3/nordpause.sh ~/nordpause.sh
 cp ~/deb12-i3/nordpnp.sh ~/nordpnp.sh
 cp ~/deb12-i3/nordresume.sh ~/nordresume.sh
+cp ~/deb12-i3/i3config.sh ~/i3config.sh
