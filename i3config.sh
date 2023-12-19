5939a54d667378dce7339aafd95477cd39431f0b
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2023-12-19 00:20:35 -0500

    Update i3config.sh

--- i3config.sh
+++ i3config.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cp ~/.config/i3/config ~/.config/i3/configbkps/config-$(date -u +'%Y%m%d-%H%M%SZ')
+cp ~/.config/i3/config ~/.config/i3/configbkps/config-$(date -u +'%Y%m%d-%H%M%SZ') &
+sleep 1
 konsole --noclose -e nano ~/.config/i3/config &
 sleep 1
 i3-msg fullscreen
