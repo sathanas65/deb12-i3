f237d8626444dd5574c01bb265e31cd3d6de11cf
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2023-12-18 19:18:41 -0500

    Update importscripts.sh

--- importscripts.sh
+++ importscripts.sh
@@ -5,12 +5,3 @@ cp ~/deb12-i3/nordpnp.sh ~/nordpnp.sh
 cp ~/deb12-i3/nordresume.sh ~/nordresume.sh
 cp ~/deb12-i3/i3config.sh ~/i3config.sh
 
-## Uncomment below if you are installing on kvm/qemu guest and want lightdm and i3 to auto load in 1080p
-# sudo cp ~/deb12-i3/virtual-display.sh /usr/share/virtual-display.sh
-# sudo cp ~/deb12-i3/lightdm.conf /etc/lightdm/lightdm.conf
-# sudo chmod +x /usr/share/virtual-display.sh
-# sudo chmod +x /etc/lightdm/lightdm.conf
-
-## Uncomment below if you are installing on kvm/qemu guest and want to use mutiple screens
-#cp ~/deb12-i3/vm-dual-display.sh ~/vm-dual-display.sh
-#cp ~/deb12-i3/vm-single-display.sh ~/vm-single-display.sh
