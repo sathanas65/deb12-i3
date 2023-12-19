c5569686b9177dbbb2ab3f5601ee3fa8b78bb0b4
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2023-12-19 01:13:36 -0500

    Update vm-copyconf.sh

--- vm-copyconf.sh
+++ vm-copyconf.sh
@@ -10,6 +10,7 @@ cp -r config/i3/ ~/.config
 cp -r config/dunst/ ~/.config
 cp -r config/terminator/ ~/.config
 cp -r config/rofi/ ~/.config
+cp -r config/konsole/ ~/.local/share
 chmod +x ~/.config/i3/autostart.sh
 cd ~/.config/i3/
 rm barhidden
