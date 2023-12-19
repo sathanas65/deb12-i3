e73c19e059979eec3ec4aab29acba64174ffc039
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2023-12-19 00:19:44 -0500

    Update help.sh

--- help.sh
+++ help.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-konsole --noclose -e nano keymap.txt &
+konsole --noclose -e nano ~/keymap.txt &
 sleep 1
 i3-msg floating toggle &
-sleep 1 
-i3-msg resize set [width] 50 px [height] 50 px &
-sleep 1
-i3-msg move [absolute] position center
-
+no_focus [window_role="pop-up"] &
