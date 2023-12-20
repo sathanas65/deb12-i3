f4dbf783728f9d810361bcbdfd3247fd5c5cbf47
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2023-12-20 16:13:29 -0500

    Update vm-copyconf.sh

--- vm-copyconf.sh
+++ vm-copyconf.sh
@@ -7,10 +7,10 @@ cd deb12-i3
 
 cp -r config/backgrounds/ ~/.config
 cp -r config/i3/ ~/.config
-cp -r config/dunst/ ~/.config
+#cp -r config/dunst/ ~/.config
 cp -r config/terminator/ ~/.config
 cp -r config/rofi/ ~/.config
-cp -r config/konsole/ ~/.local/share
+#cp -r config/konsole/ ~/.local/share
 chmod +x ~/.config/i3/autostart.sh
 cd ~/.config/i3/
 rm config
