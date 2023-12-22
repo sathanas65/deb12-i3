2e754a1ea5b1b62868f8e3b577fae8401e685f80
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2023-12-22 00:42:47 -0500

    Update vm-copyconf.sh

--- vm-copyconf.sh
+++ vm-copyconf.sh
@@ -6,7 +6,7 @@ cd deb12-i3
 
 cp -r config/backgrounds/ ~/.config
 cp -r config/i3/ ~/.config
-#cp -r config/dunst/ ~/.config
+cp -r config/dunst/ ~/.config
 cp -r config/terminator/ ~/.config
 cp -r config/rofi/ ~/.config
 cp -r config/volumeicon ~/.config
