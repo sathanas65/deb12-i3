0098b889b8791a9a92dad9d1393e5a2e98f84f09
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2023-12-22 00:41:54 -0500

    Update importscripts.sh

--- importscripts.sh
+++ importscripts.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
-cp ~/deb12-i3/nordconnect.sh ~/nordconnect.sh
+cd ~/deb12-i3
+cp nordconnect.sh ~/nordconnect.sh
 sudo chmod +x ~/nordconnect.sh
-cp ~/deb12-i3/norddouble.sh ~/norddouble.sh
+cp norddouble.sh ~/norddouble.sh
 sudo chmod +x ~/norddouble.sh
-cp ~/deb12-i3/nordpause.sh ~/nordpause.sh
+cp nordpause.sh ~/nordpause.sh
 sudo chmod +x ~/nordpause.sh
-cp ~/deb12-i3/nordpnp.sh ~/nordpnp.sh
+cp nordpnp.sh ~/nordpnp.sh
 sudo chmod +x ~/nordpnp.sh
-cp ~/deb12-i3/nordresume.sh ~/nordresume.sh
+cp nordresume.sh ~/nordresume.sh
 sudo chmod +x ~/nordresume.sh
-cp ~/deb12-i3/i3config.sh ~/i3config.sh
+cp i3config.sh ~/i3config.sh
 sudo chmod +x ~/i3config.sh
-cp ~/deb12-i3/keymap.txt ~/keymap.txt
-cp ~/deb12-i3/help.sh ~/help.sh
+cp keymap.txt ~/keymap.txt
+cp help.sh ~/help.sh
 sudo chmod +x ~/help.sh
-cp ~/deb12-i3/background.sh ~/background.sh
+cp background.sh ~/background.sh
 sudo chmod +x ~/background.sh
-cp ~/deb12-i3/togglebar.sh ~/togglebar.sh
+cp togglebar.sh ~/togglebar.sh
 sudo chmod +x ~/togglebar.sh
 
 ## These scripts are customized for my personal display hardware. Edit as needed.
-cp ~/deb12-i3/right.sh ~/right.sh
+cp right.sh ~/right.sh
 sudo chmod +x ~/right.sh
-cp ~/deb12-i3/dual.sh ~/dual.sh
+cp dual.sh ~/dual.sh
 sudo chmod +x ~/dual.sh
-cp ~/deb12-i3/tv.sh ~/tv.sh
+cp tv.sh ~/tv.sh
 sudo chmod +x ~/tv.sh
