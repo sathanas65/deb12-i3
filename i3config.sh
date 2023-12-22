a0c946c4bb9cb1182a0d375fc3b9433b0cc665f9
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2023-12-22 13:02:31 -0500

    Delete i3config.sh

--- i3config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cp ~/.config/i3/config ~/.config/i3/configbkps/config-$(date -u +'%Y%m%d-%H%M%SZ') &
-konsole --profile "Profile 1" --noclose -e nano ~/.config/i3/config --title "i3config" &
-sleep 1
-i3-msg fullscreen
-i3-msg fullscreen
