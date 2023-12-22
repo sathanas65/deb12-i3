ad559a588ddc6e89c17c25b7ae3d97df26c7e519
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2023-12-22 13:31:30 -0500

    Update help.sh

--- scripts/help.sh
+++ scripts/help.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-konsole --profile "Profile 1" --noclose -e nano ~/keymap.txt --title "KeymapHelp" &
+konsole --profile "Profile 1" --noclose -e nano ~/scripts/keymap.txt --title "KeymapHelp" &
 sleep 1
 i3-msg fullscreen
 i3-msg fullscreen
