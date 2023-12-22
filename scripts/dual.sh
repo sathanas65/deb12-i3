fa6ebb17a3f65ab91fdb6a5345855482df4d3b5b
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2023-12-22 13:31:05 -0500

    Update dual.sh

--- scripts/dual.sh
+++ scripts/dual.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 xrandr --size 3840x1080 --output DP-0 --primary --mode 1920x1080 --pos 0x0 --output DVI-D-0 --mode 1920x1080 --pos 1920x0 --output HDMI-0 --off
-sh background.sh
+sh scripts/background.sh
 
