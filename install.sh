7fd37a5d6af58724d374f84fdb59d11527e25bff
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2023-12-22 09:34:46 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -175,8 +175,6 @@ sudo apt install -y i3
 sudo apt install -y lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
 sudo systemctl enable lightdm
 
-
-
 # import scripts and configs for physical device (comment these out if you are installing on vm)
 #sh ~/deb12-i3/copyconf.sh
 #sh ~/deb12-i3/importscripts.sh
