abae517935f874b0ea7f8286478daa51343951c4
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2023-12-22 19:03:55 -0500

    Update autostart.sh

--- config/i3/autostart.sh
+++ config/i3/autostart.sh
@@ -1,13 +1,16 @@
 #configure display
 #xrandr --output Virtual-1 --mode 1920x1080
+/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
 
 #enable picom to support transparency
 picom -b &
-
+dunst &
 numlokx on &
 nm-applet &
 volumeicon &
 copyq &
 
 #configure backgroung
-sh scripts/background.sh
+sh scripts/background.sh &
+sleep 1
+sh scripts/nordconnect.sh &
