0e97e83aca1d432045e8b330998ff33394712c7e
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2023-12-23 03:49:46 -0500

    Update nordresume.sh

--- scripts/nordresume.sh
+++ scripts/nordresume.sh
@@ -2,7 +2,8 @@
 
 # Connect to NordVPN and capture the output
 connect_output=$(nordvpn connect 2>&1)
-connect_exit_status=$?
+connect_exit_status=$? &
+i3-msg mode "default"
 
 # Disable the killswitch and capture the output
 killswitch_output=$(nordvpn set killswitch enabled 2>&1)
