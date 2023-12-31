9422069762ef0fb28e4ba4b083bb5f77d3bb5880
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2023-12-31 13:09:43 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -207,6 +207,14 @@ sudo apt install -y spice-vdagent
 # sudo usermod -aG libvirt “$(whoami)”
 # sudo usermod -aG kvm “$(whoami)”
 
+# dev tools
+# vscode
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt update
+sudo apt install code
+
 # user directories
 xdg-user-dirs-update
 
