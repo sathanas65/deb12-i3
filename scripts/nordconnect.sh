879a5c7f9aef780112a02aa9b4d3ea921d9dd4de
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2023-12-31 02:03:11 -0500

    Update nordconnect.sh

--- scripts/nordconnect.sh
+++ scripts/nordconnect.sh
@@ -4,10 +4,17 @@
 connect_output=$(nordvpn connect 2>&1)
 connect_exit_status=$?
 
-# Disable the killswitch and capture the output
-killswitch_output=$(nordvpn set killswitch enabled 2>&1)
-killswitch_exit_status=$?
-
-# Send a notification with the output of the both commands
 notify-send "NordVPN Connection" "$connect_output - Exit Status: $connect_exit_status"
-notify-send "$killswitch_output - Exit Status: $killswitch_exit_status" 
+
+# Check if the output contains "you are not logged in"
+if [[ $connect_output == *"You are not logged in"* ]]; then
+	# Disable the killswitch
+	nordvpn set killswitch disabled
+    # Run the nordlogin.sh script
+    bash scripts/nordlogin.sh
+else 
+	# Enable the killswitch and capture the output
+	killswitch_output=$(nordvpn set killswitch enabled 2>&1)
+	killswitch_exit_status=$?
+	notify-send "$killswitch_output - Exit Status: $killswitch_exit_status" 
+fi
