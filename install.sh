86e43da84f36dd05c5f4dda6341bdb12efa05670
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2023-12-31 15:33:05 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -122,7 +122,14 @@ sudo snap install core
 # gui text editor
 sudo apt install -y geany
 # notepad++ (FOSS Windows application, uses Wine to run on Linux) snap run notepad-plus-plus (command to launch)
-sudo snap install notepad-plus-plus 
+#sudo snap install notepad-plus-plus 
+# brackets
+sudo snap install brackets --classic
+# sublime text
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update
+sudo apt-get install -y sublime-text
 
 # system management
 # cockpit (admin web console)
