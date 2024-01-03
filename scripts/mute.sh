9757a4e124233ff368ebe4926781384a9bbbce3a
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2024-01-03 01:52:14 -0500

    Update mute.sh

--- scripts/mute.sh
+++ scripts/mute.sh
@@ -7,7 +7,7 @@ DEFAULT_SINK=$(pactl info | grep 'Default Sink' | cut -d' ' -f3)
 MUTE_STATUS=$(pactl list sinks | grep -A 15 "Name: $DEFAULT_SINK" | grep 'Mute:' | awk '{print $2}')
 
 if [ "$MUTE_STATUS" = "yes" ]; then
-    pactl set-sink-mute @DEFAULT_SINK@ toggle &&  dunstify "Mute On"
-else
     pactl set-sink-mute @DEFAULT_SINK@ toggle &&  dunstify "Mute Off"
+else
+    pactl set-sink-mute @DEFAULT_SINK@ toggle &&  dunstify "Mute On"
 fi
