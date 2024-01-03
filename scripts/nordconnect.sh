1f79a4e8e0e03b3cc2a15f5e7eea03790971a48b
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2024-01-02 23:03:31 -0500

    Update nordconnect.sh

--- scripts/nordconnect.sh
+++ scripts/nordconnect.sh
@@ -4,10 +4,12 @@
 connect_output=$(nordvpn connect 2>&1)
 connect_exit_status=$?
 
-notify-send "NordVPN Connection" "$connect_output - Exit Status: $connect_exit_status"
-
 # Check if the output contains "you are not logged in"
 if [[ $connect_output == *"You are not logged in"* ]]; then
+	notify-send --urgency=critical "NordVPN Connection:
+	YOU ARE NOT LOGGED IN AND KILLSWITCH IS DISABLED!
+	YOUR REAL IP IS VISIBLE!"
+	
 	# Disable the killswitch
 	nordvpn set killswitch disabled
     # Run the nordlogin.sh script
@@ -16,5 +18,7 @@ else
 	# Enable the killswitch and capture the output
 	killswitch_output=$(nordvpn set killswitch enabled 2>&1)
 	killswitch_exit_status=$?
+	notify-send "NordVPN Connection:" "$connect_output - Exit Status: $connect_exit_status"
+	sleep 4
 	notify-send "$killswitch_output - Exit Status: $killswitch_exit_status" 
 fi
