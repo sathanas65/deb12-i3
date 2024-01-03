91bb74d5679316950d19fdcf520529dcb51c332f
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2024-01-03 00:46:11 -0500

    Create mute.sh

---
+++ scripts/mute.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Get the default sink name
+DEFAULT_SINK=$(pactl info | grep 'Default Sink' | cut -d' ' -f3)
+
+# Get the mute status of the default sink
+MUTE_STATUS=$(pactl list sinks | grep -A 15 "Name: $DEFAULT_SINK" | grep 'Mute:' | awk '{print $2}')
+
+if [ "$MUTE_STATUS" = "yes" ]; then
+    pactl set-sink-mute @DEFAULT_SINK@ toggle &&  dunstify "Mute On"
+else
+    pactl set-sink-mute @DEFAULT_SINK@ toggle &&  dunstify "Mute Off"
+fi
