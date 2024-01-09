d3c74dd57410c298e28063f62037d0edfe2bc59f
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2024-01-09 01:33:10 -0500

    Add files via upload

---
+++ scripts/firewall-boot-status.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export DISPLAY=:0
+export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u)/bus"
+
+# Get Firewall status
+status_output=$(sudo ufw status 2>&1)
+
+if [[ $status_output == *"Status: inactive"* ]]; then
+	# Continuously check the firewall status
+	while [[ $new_status_output != *"Status: active"* ]]; do
+		
+		warning=$(echo -e "UFW WARNING:\nYOUR FIREWALLIS DISABLED!\nYOUR NETWORK PORTS\nARE ALL OPEN!")
+		notify-send --urgency=critical "$warning" -t 0
+		unset $status_output
+    
+		# Wait for a bit before checking again
+		sleep 5
+		# Check UFW status
+		new_status_output=$(sudo ufw status 2>&1)
+	done
+	tmux kill-session -t "$(tmux display-message -p '#S')"
