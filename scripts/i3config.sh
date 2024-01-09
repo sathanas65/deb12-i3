130d4d29019fb60f1f4143fb5b56e26b6f9389b2
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2024-01-09 01:33:49 -0500

    Add files via upload

--- scripts/i3config.sh
+++ scripts/i3config.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-cp ~/.config/i3/config ~/.config/i3/configbkps/config-$(date -u +'%Y%m%d-%H%M%SZ') &
-konsole --profile "Profile 1" --noclose -e nano ~/.config/i3/config --title "i3config" &
-sleep 1
-i3-msg fullscreen
-i3-msg fullscreen
+
+cp ~/.config/i3/config ~/.config/i3/configbkps/config-$(date -u +'%Y%m%d-%H%M%SZ')
+geany ~/.config/i3/config &
+i3-msg mode "default"
+
+#cp ~/.config/i3/config ~/.config/i3/configbkps/config-$(date -u +'%Y%m%d-%H%M%SZ')
+#i3-msg workspace 'Config'
+#sleep 0.5  # Short delay to ensure the workspace switch occurs
+#geany ~/.config/i3/config
+
+
+
