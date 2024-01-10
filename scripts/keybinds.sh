97bba1dc7b2391fa0bf0def10e3635521e3aee78
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2024-01-09 23:28:41 -0500

    Update keybinds.sh

--- scripts/keybinds.sh
+++ scripts/keybinds.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp ~/.config/i3/config.d/keybinds.conf ~/.config/i3/configbkps/keybinds-$(date -u +'%Y%m%d-%H%M%SZ')
+cp ~/.config/i3/config.d/keybinds.conf ~/.config/i3/configbkps/keybinds.conf-$(date -u +'%Y%m%d-%H%M%SZ')
 geany ~/.config/i3/config.d/keybinds.conf &
 i3-msg mode "default"
