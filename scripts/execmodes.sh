eab651c010f189044fcf4cecac2e0db570ff8981
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2024-01-09 23:24:00 -0500

    Create execmodes.sh

---
+++ scripts/execmodes.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp ~/.config/i3/config.d/execmodes.conf ~/.config/i3/configbkps/execmodes-$(date -u +'%Y%m%d-%H%M%SZ')
+geany ~/.config/i3/config.d/execmodes.conf &
+i3-msg mode "default"
