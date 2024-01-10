7c83016f23d4edcf3b0736053bd0752d42f483b4
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2024-01-09 23:27:39 -0500

    Update baremetal.sh

--- scripts/baremetal.sh
+++ scripts/baremetal.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp ~/.config/i3/config.d/baremetal.conf ~/.config/i3/configbkps/baremetal-$(date -u +'%Y%m%d-%H%M%SZ')
+cp ~/.config/i3/config.d/baremetal.conf ~/.config/i3/configbkps/baremetal.conf-$(date -u +'%Y%m%d-%H%M%SZ')
 geany ~/.config/i3/config.d/baremetal.conf &
 i3-msg mode "default"
