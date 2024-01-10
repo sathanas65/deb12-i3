227380ec2ff72124164d1badc6c874887926189a
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2024-01-09 22:02:00 -0500

    Update and rename virtual-display.sh to display.sh

---
+++ display.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+#xrandr --output Virtual-1 --mode 1920x1080 
+xrandr --size 3840x1080 --output Virtual-1 --primary --mode 1920x1080 --pos 0x0 --output Virtual-2 --mode 1920x1080 --pos 1920x0
+sh scripts/background.sh
+
--- virtual-display.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-xrandr --output Virtual-1 --mode 1920x1080 
