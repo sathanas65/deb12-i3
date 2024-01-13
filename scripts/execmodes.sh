05fc2098f1eb0661bd7ed287795dd8f901039350
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2024-01-13 01:20:20 -0500

    Delete scripts/execmodes.sh

--- scripts/execmodes.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp ~/.config/i3/config.d/execmodes.conf ~/.config/i3/configbkps/execmodes.conf-$(date -u +'%Y%m%d-%H%M%SZ')
-geany ~/.config/i3/config.d/execmodes.conf &
-i3-msg mode "default"
