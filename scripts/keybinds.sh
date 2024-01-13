13675059a083d5373cd210aac3bad9b329d1d175
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2024-01-13 01:20:42 -0500

    Delete scripts/keybinds.sh

--- scripts/keybinds.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp ~/.config/i3/config.d/keybinds.conf ~/.config/i3/configbkps/keybinds.conf-$(date -u +'%Y%m%d-%H%M%SZ')
-geany ~/.config/i3/config.d/keybinds.conf &
-i3-msg mode "default"
