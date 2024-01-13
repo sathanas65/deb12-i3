f4fb2730be23362427865b1cb676993af0a60c97
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2024-01-13 01:21:33 -0500

    Delete scripts/workspaces.sh

--- scripts/workspaces.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp ~/.config/i3/config.d/workspaces.conf ~/.config/i3/configbkps/workspaces.conf-$(date -u +'%Y%m%d-%H%M%SZ')
-geany ~/.config/i3/config.d/workspaces.conf &
-i3-msg mode "default"
