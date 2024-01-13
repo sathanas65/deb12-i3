121bf3b46ff6606dba2ab55711d4132d83d0e1e5
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2024-01-13 01:20:10 -0500

    Delete scripts/containers.sh

--- scripts/containers.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp ~/.config/i3/config.d/containers.conf ~/.config/i3/configbkps/containers.conf-$(date -u +'%Y%m%d-%H%M%SZ')
-geany ~/.config/i3/config.d/containers.conf &
-i3-msg mode "default"
