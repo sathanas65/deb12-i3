f7bf239ca8113628a9c4e032a9adbb64ff17bf71
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2024-01-17 22:02:21 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -592,7 +592,7 @@ sudo curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc https://repository.mull
 check_status
 echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$( dpkg --print-architecture )] https://repository.mullvad.net/deb/stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/mullvad.list
 check_status
-sudo apt-get update
+sudo apt update
 check_status
 sudo apt-get install -y mullvad-vpn
 check_status
