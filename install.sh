f7ab356bccb63e7286ac6dd3270343393ea0f235
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2024-01-18 00:14:55 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -428,7 +428,7 @@ sudo apt-get update && sudo apt-get install -y codium
 # pycharm
 curl -s https://s3.eu-central-1.amazonaws.com/jetbrains-ppa/0xA6E8698A.pub.asc | gpg --dearmor | sudo tee /usr/share/keyrings/jetbrains-ppa-archive-keyring.gpg > /dev/null
 echo "deb [signed-by=/usr/share/keyrings/jetbrains-ppa-archive-keyring.gpg] http://jetbrains-ppa.s3-website.eu-central-1.amazonaws.com any main" | sudo tee /etc/apt/sources.list.d/jetbrains-ppa.list > /dev/null
-sudo update
+sudo apt-get update
 sudo apt-get install -y pycharm-community
 
 #sudo -v
