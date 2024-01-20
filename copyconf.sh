7dced8ac0319853508417b69b12ad5e7fdbf267c
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2024-01-20 15:22:01 -0500

    Update copyconf.sh

--- copyconf.sh
+++ copyconf.sh
@@ -30,6 +30,7 @@ cp config/kcalcrc ~/.config
 cp config/konsolerc ~/.config
 cp config/krusaderrc ~/.config
 sudo cp -r config/Sweet-Dark-v40 /usr/share/themes
+sudo cp -r candy-icons /usr/share/icons
 
 chmod +x ~/.config/i3/autostart.sh
 chmod +x ~/.config/i3blocks/cpu/cpu_info.sh
