db62b1d50773520bf3284dd534420218a7e97d7f
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2024-01-21 16:42:24 -0500

    Update copyconf.sh

--- copyconf.sh
+++ copyconf.sh
@@ -6,37 +6,54 @@ sudo cp -r candy-icons /usr/share/icons
 cd
 cd deb12-i3
 
+# import scripts
 cp -r scripts/ ~/
+
+# import backgrounds
 cp -r config/backgrounds/ ~/.config
+
+# import window manager and bar configs
 cp -r config/i3/ ~/.config
 cp -r config/i3blocks/ ~/.config
+
+# import notification daemon config
 cp -r config/dunst/ ~/.config
-cp -r config/chromium/ ~/.config
-cp -r config/Brave-Browser/ ~/.config
-cp -r config/terminator/ ~/.config
+
+# import app launcher config (optional - ensures dark theme works out of the box)
 cp -r config/rofi/ ~/.config
-cp -r config/volumeicon ~/.config
+
+# import browser configs (optional - ensures dark theme and dark reader extension work out of the box)
+cp -r config/chromium/ ~/.config
+cp -r .mozilla ~
+cp -r config/Brave-Browser/ ~/.config
+
+# import terminal configs (optional - ensures dark theme and transparancy work out of the box)
+cp -r config/terminator/ ~/.config
+cp -r config/konsole/ ~/.local/share
+cp config/konsolerc ~/.config
+
+# import volume icon config  (necessary?)
+#cp -r config/volumeicon ~/.config
+
+# Import themes and themed configs
 cp -r config/xfce4 ~/.config
-#cp -r config/Brackets ~/.config
-cp -r config/copyq ~/.config
-cp -r config/galculator ~/.config
 cp -r config/gtk-3.0 ~/.config
 cp -r config/gtk-4.0 ~/.config
-cp -r config/xfce4 ~/.config
-#cp -r config/sublime-text ~/.config
-cp -r .mozilla ~
-cp -r config/konsole/ ~/.local/share
 cp config/QtProject.conf ~/.config
-cp config/kcalcrc ~/.config
-cp config/konsolerc ~/.config
-cp config/krusaderrc ~/.config
 sudo cp -r config/Sweet-Dark-v40 /usr/share/themes
+cp -r config/copyq ~/.config
+cp -r config/galculator ~/.config
+cp config/kcalcrc ~/.config
+#cp config/krusaderrc ~/.config
 
+#import editor configs - optional
+#cp -r config/sublime-text ~/.config 
+#cp -r config/Brackets ~/.config
+
+# make scripts executable
 chmod +x ~/.config/i3/autostart.sh
 chmod +x ~/.config/i3blocks/cpu/cpu_info.sh
 chmod +x ~/.config/i3blocks/battery/battery_info.sh
-
-# make scripts executable
+chmod +x ~/.config/i3blocks/weather/weather.sh
+chmod +x ~/.config/i3blocks/weather/weather.py
 sudo chmod +x ~/scripts/*.sh
-
-
