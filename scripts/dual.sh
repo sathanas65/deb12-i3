27508ca59f273a3536ec63d24d9bfc4564176b73
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2024-09-07 19:15:04 -0400

    Update dual.sh
    Added rotate = normal tag to return to landscape mode

--- scripts/dual.sh
+++ scripts/dual.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-xrandr --size 3840x1080 --output DP-0 --primary --mode 1920x1080 --pos 0x0 --output DVI-D-0 --mode 1920x1080 --pos 1920x0 --output HDMI-0 --off
+xrandr --size 3840x1080 --output DisplayPort-1 --primary --mode 1920x1080 --rotate normal --pos 0x0 --set TearFree on --output DVI-D-0 --mode 1920x1080 --pos 1920x0 --set TearFree on --output HDMI-A-0 --off
 sh scripts/background.sh
-
