9a2c384ac283b93939725bafbf7764eb40c830a5
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2024-09-07 18:35:48 -0400

    Update port.sh

--- scripts/port.sh
+++ scripts/port.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-xrandr --size 3000x1920 --output DP-0 --primary --mode 1080x1920 --pos 0x0 --output DVI-D-0 --mode 1920x1080 --pos 1080x420 --output HDMI-0 --off
+xrandr --size 3000x1920 --output DP-0 --primary --mode 1920x1080 --rotate left --pos 0x0 --output DVI-D-0 --mode 1920x1080 --pos 1080x420 --output HDMI-0 --off
 sh scripts/background.sh
