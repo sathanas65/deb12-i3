5b6c595ba14531cb5aa01e16f1870b21767442b8
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2024-09-07 19:50:17 -0400

    Update install.sh
    Added command to add user to vboxusers group

--- install.sh
+++ install.sh
@@ -404,6 +404,7 @@ git clone https://github.com/EliverLara/candy-icons
 # virtualbox extension pack
 #wget https://download.virtualbox.org/virtualbox/7.0.10/Oracle_VM_VirtualBox_Extension_Pack-7.0.10.vbox-extpack
 #echo y | sudo vboxmanage extpack install Oracle_VM_VirtualBox_Extension_Pack-7.0.10.vbox-extpack
+#sudo usermod -a -G vboxusers $USER
 
 ### graphical user interface
 
