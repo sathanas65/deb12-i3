7ddb29ad8f979072f1cfe7db9c528cce20c49586
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2024-09-07 19:16:40 -0400

    Update right.sh

--- scripts/right.sh
+++ scripts/right.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-xrandr --size 1920x1080 --output DVI-D-0 --primary --mode 1920x1080 --pos 0x0 --output DP-0 --off --output HDMI-0 --off
+xrandr --size 1920x1080 --output DVI-D-0 --primary --mode 1920x1080 --pos 0x0 --set TearFree on --output DisplayPort-1 --off --output HDMI-A-0 --off
 sh scripts/background.sh
