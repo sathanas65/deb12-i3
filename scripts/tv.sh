002b31519b347003c0f4448f72cc205d26abc340
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2024-09-07 23:23:58 -0400

    Update tv.sh
    added rotate=normal flag to restore landscape mode

--- scripts/tv.sh
+++ scripts/tv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-xrandr --size 3840x3240 --output HDMI-A-0 --mode 3840x2160 --rotate normal --pos 0x0 --set TearFree on --output DisplayPort-1 --primary --mode 1920x1080 --pos 0x2160 --set TearFree on --output DVI-D-0 --mode 1920x1080 --pos 1920x2160 --set TearFree on
+xrandr --size 3840x3240 --output HDMI-A-0 --mode 3840x2160 --rotate normal --pos 0x0 --set TearFree on --output DisplayPort-1 --primary --mode 1920x1080 --rotate normal --pos 0x2160 --set TearFree on --output DVI-D-0 --mode 1920x1080 --pos 1920x2160 --set TearFree on
 sh scripts/background.sh
 i3-msg focus output HDMI-A-0
 i3-msg workspace "TV"
