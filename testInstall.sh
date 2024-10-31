fb53d2c6d1aad612527a73c5b9ff31a4bd4a567b
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2024-10-31 16:35:12 -0400

    Update testInstall.sh

--- testInstall.sh
+++ testInstall.sh
@@ -347,7 +347,7 @@ sudo usermod -aG nordvpn $USER
 sudo curl -fsSLo /usr/share/keyrings/mullvad-keyring.asc https://repository.mullvad.net/deb/mullvad-keyring.asc
 echo "deb [signed-by=/usr/share/keyrings/mullvad-keyring.asc arch=$( dpkg --print-architecture )] https://repository.mullvad.net/deb/stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/mullvad.list
 sudo apt-get update
-sudo apt-get install mullvad-browser
+sudo apt-get install mullvad-vpn
 
 
 # personal finance
