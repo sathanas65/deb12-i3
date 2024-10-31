887fdd135d6dedab726c86a8536eb5fdeaef9201
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2024-10-31 03:34:34 -0400

    Update testInstall.sh

--- testInstall.sh
+++ testInstall.sh
@@ -118,19 +118,9 @@ sudo apt-get install -y galculator
 #sudo apt-get install -y brave-browser
 
 # librewolf browser
-distro=$(if echo "bookworm" | grep -q " $(lsb_release -sc) "; then echo $(lsb_release -sc); else echo focal; fi)
-wget -O- https://deb.librewolf.net/keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/librewolf.gpg
-sudo tee /etc/apt/sources.list.d/librewolf.sources << EOF > /dev/null
-Types: deb
-URIs: https://deb.librewolf.net
-Suites: $distro
-Components: main
-Architectures: amd64
-Signed-By: /usr/share/keyrings/librewolf.gpg
-EOF
-
-sudo apt-get update
-sudo apt-get install -y librewolf
+sudo apt update && sudo apt install extrepo -y
+sudo extrepo enable librewolf
+sudo apt update && sudo apt install librewolf -y
 
 # tor browser
 #sudo apt-get install -y torbrowser-launcher 
