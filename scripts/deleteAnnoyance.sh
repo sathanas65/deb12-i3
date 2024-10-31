e92d3c4a580def74c02127524817c4a3b87537b6
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2024-10-31 02:38:37 -0400

    Delete scripts/deleteAnnoyance.sh

--- scripts/deleteAnnoyance.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -r ~/.local/share/kwalletd
-rm ~/.config/i3/autostart.sh
-cp ~/.config/i3/autostart.sh.after ~/.config/i3/autostart.sh
-rm ~/.config/i3/autostart.sh.after
-rm ~/scripts/deleteAnnoyance.sh
