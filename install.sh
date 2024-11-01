5bb004f471ac795662792edaac6dc956de5abb92
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2024-10-31 23:03:53 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -230,7 +230,7 @@ sudo snap install pinta
 sudo cp /var/lib/snapd/desktop/applications/pinta_pinta.desktop ~/.local/share/applications/
 
 # zip utilities
-sudo apt-get install -y tar gzip
+sudo apt-get install -y tar gzip p7zip-full
 
 # backup manager
 # timeshit gui front end for rsync
