05770c71a74af098e1e33b88acb827ae9af15c9c
sathanas65 <53740104+sathanas65@users.noreply.github.com>
2024-11-02 22:15:32 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -278,11 +278,11 @@ sudo apt-get install -y tar gzip p7zip-full
 #sudo apt-get install -y transmission
 
 # signal encrypted messaging
-#wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
-#cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
-#echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
-#  sudo tee /etc/apt/sources.list.d/signal-xenial.list
-#sudo apt-get update && sudo apt-get install -y signal-desktop
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
+  sudo tee /etc/apt/sources.list.d/signal-xenial.list
+sudo apt-get update && sudo apt-get install -y signal-desktop
 
 # screen recorder
 #sudo apt-get install -y simplescreenrecorder
